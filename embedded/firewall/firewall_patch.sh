d9e743d4d654319023dc7c4758ca09ea228288e5
crazytypewriter <inna.ded@gmail.com>
2025-04-23 19:17:20 +0300

    mem leak fix, disable singbox graf, bump singbox version, add ipv6 support

--- embedded/firewall/firewall_patch.sh
+++ embedded/firewall/firewall_patch.sh
@@ -11,23 +11,16 @@ echo "#!/bin/sh
           fi
 
           ip rule list | grep -q \"fwmark 0x2 lookup vpn\" || ip rule add fwmark 0x2 lookup vpn
-          iptables -t mangle -C OUTPUT -m set --match-set vpn_domains dst -j MARK --set-mark 0x2 2>/dev/null || \
-              iptables -t mangle -A OUTPUT -m set --match-set vpn_domains dst -j MARK --set-mark 0x2
-          iptables -t mangle -C PREROUTING -m set --match-set vpn_domains dst -j MARK --set-mark 0x2 2>/dev/null || \
-              iptables -t mangle -A PREROUTING -m set --match-set vpn_domains dst -j MARK --set-mark 0x2
-          iptables -C FORWARD -m mark --mark 0x2 -j ACCEPT 2>/dev/null || \
-              iptables -I FORWARD -m mark --mark 0x2 -j ACCEPT
-          iptables -t nat -C POSTROUTING -o tun0 -j SNAT --to-source 172.16.250.1 2>/dev/null || \
-              iptables -t nat -A POSTROUTING -o tun0 -j SNAT --to-source 172.16.250.1
+          iptables -t mangle -C OUTPUT -m set --match-set vpn_domains dst -j MARK --set-mark 0x2 2>/dev/null || iptables -t mangle -A OUTPUT -m set --match-set vpn_domains dst -j MARK --set-mark 0x2
+          iptables -t mangle -C PREROUTING -m set --match-set vpn_domains dst -j MARK --set-mark 0x2 2>/dev/null || iptables -t mangle -A PREROUTING -m set --match-set vpn_domains dst -j MARK --set-mark 0x2
+          iptables -C FORWARD -m mark --mark 0x2 -j ACCEPT 2>/dev/null || \iptables -I FORWARD -m mark --mark 0x2 -j ACCEPT
+          iptables -t nat -C POSTROUTING -o tun0 -j SNAT --to-source 172.16.250.1 2>/dev/null || iptables -t nat -A POSTROUTING -o tun0 -j SNAT --to-source 172.16.250.1
 
-          #ip -6 rule list | grep -q \"fwmark 0x2 lookup vpn\" || ip -6 rule add fwmark 0x2 lookup vpn
+          ip -6 rule list | grep -q \"fwmark 0x2 lookup vpn\" || ip -6 rule add fwmark 0x2 lookup vpn
 
-          ip6tables -t mangle -C OUTPUT -m set --match-set vpn_domains6  dst -j MARK --set-mark 0x2 2>/dev/null || \
-              ip6tables -t mangle -A OUTPUT -m set --match-set vpn_domains6 dst -j MARK --set-mark 0x2
-          ip6tables -t mangle -C PREROUTING -m set --match-set vpn_domains6 dst -j MARK --set-mark 0x2 2>/dev/null || \
-              ip6tables -t mangle -A PREROUTING -m set --match-set vpn_domains6 dst -j MARK --set-mark 0x2
-          ip6tables -t mangle -C PREROUTING -m set --match-set vpn_domains6 dst -j MARK --set-mark 0x2 2>/dev/null || \
-              ip6tables -t mangle -A PREROUTING -m set --match-set vpn_domains6 dst -j MARK --set-mark 0x2
+          ip6tables -t mangle -C OUTPUT -m set --match-set vpn_domains6  dst -j MARK --set-mark 0x2 2>/dev/null || ip6tables -t mangle -A OUTPUT -m set --match-set vpn_domains6 dst -j MARK --set-mark 0x2
+          ip6tables -C FORWARD -m mark --mark 0x2 -j ACCEPT 2>/dev/null || ip6tables -I FORWARD -m mark --mark 0x2 -j ACCEPT
+          ip6tables -t mangle -C PREROUTING -m set --match-set vpn_domains6 dst -j MARK --set-mark 0x2 2>/dev/null || ip6tables -t mangle -A PREROUTING -m set --match-set vpn_domains6 dst -j MARK --set-mark 0x2
           return
       }
 
